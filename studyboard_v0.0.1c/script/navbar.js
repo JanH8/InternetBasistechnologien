e52285c9045cf61afe00e46faf86e3ac8b0b987c
Leon <leon.schoenhoff@bbht.de>
2019-04-16 13:29:31 +0200

    Featuring: Navbar
    JS basiert

---
+++ studyboard_v0.0.1c/script/navbar.js
@@ -0,0 +1,14 @@
+function hidenavbtn() {
+    document.getElementById("navbtn").style.right="-50%";
+}
+function shownavbtn() {
+    document.getElementById("navbtn").style.right="20px";
+}
+function shownavbar() {
+    document.getElementById("navbar").style.left="0";
+    document.getElementById("wte").style.display="block";
+}
+function hidenavbar() {
+    document.getElementById("navbar").style.left="-100%";
+    document.getElementById("wte").style.display="none";
+}
